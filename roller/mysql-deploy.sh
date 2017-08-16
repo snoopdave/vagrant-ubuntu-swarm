7ae860874a3b23b1556697e171d6f9d8312e88af
Dave Johnson <snoopdave@gmail.com>
2017-08-16 11:58:58 -0400

    Scripts for deploying MySQL and Roller.

--- roller/mysql-deploy.sh
+++ roller/mysql-deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 docker service create \
     --name mysql \
     --network roller \
--- roller/roller-deploy.sh
+++ roller/roller-deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 docker service create \
     --name roller \
     --network roller \
